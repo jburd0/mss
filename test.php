39084f9bdaedbfa774a733e7fefbe67ce904f920
johnburd <johnburd19@gmail.com>
2014-01-22 13:44:37 -0600

    Renamed folder name of user

---
+++ test.php
@@ -0,0 +1,265 @@
+
+<!--include virtual="/detectmobilebrowser.asp" -->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<meta name="robots" content="index, follow" />
+<meta name="Description" content="We offer a bowling alley, Movie Theater, restaurant, a conference room for businesses or parties.  Number one place for family fun!">
+<meta name="keywords" content="bowling website, website template, hosting" />
+<meta name="rating" content="GENERAL" />
+<meta name="distribution" content="GLOBAL" />
+<meta name="generator" content="BPAA Webservices" />
+<title>ConnXtion Entertainment | Home</title>
+<link href="/Skins/Urban/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39644485-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script><meta http-equiv="refresh" content="120">
+<style type="text/css">
+<!--
+.HoursTable {
+	color: #FFF;
+	background: #333;
+	border: 3px solid #000;
+	-moz-border-radius: 6px;
+	-webkit-border-radius: 6px;
+        border-radius: 6px;
+        padding:10px;
+}
+-->
+</style>
+
+</head>
+<body>
+<table width="90%" border="0" cellpadding="0" cellspacing="0" align="center" class="pageWidth">
+  <tr>
+    <td valign="top"><!---- New Menu Stuff --->
+<!--[if lte IE 7]>
+<style type="text/css">
+html .jqueryslidemenu{height: 1%;} /*Holly Hack for IE7 and below*/
+</style>
+<![endif]-->
+<script type="text/javascript" src="/ScriptLibrary/jquery_142.js"></script>
+<script type="text/javascript" src="ScriptLibrary/MainNavigation/jqueryslidemenu.js"></script>
+
+
+
+<table width="100%" border="0" cellpadding="0" cellspacing="0" align="center">
+  <tr>
+    <td><table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+          <td class="BannerArea"><div id="searchBoxDiv">
+              <div id="search_box">
+                <form id="search_form" method="get" action="/Search.asp">
+                  <input name="SearchFor" class="swap_value searchBoxTextField" id="s" value="Search" onFocus="this.select()" onBlur="this.value=!this.value?'Search':this.value;" onChange="javascript:while(''+this.value.charAt(this.value.length-1)==' ')this.value=this.value.substring(0,this.value.length-1);" />
+                  <input type="image" src="/images/btn_search_box.gif" width="27" height="24" id="go" alt="Search" title="Search" class="searchBoxButton" />
+                </form>
+              </div>
+              <div style="clear:both"></div>
+              <div id="SocialBookmarks2">
+                <!-- AddThis Button BEGIN -->
+                Share: <a href="#" class="SocialButtons addthis_button_facebook"><img src="images/socialIcons/48x48/facebook.png" alt="Facebook" width="24" height="24" border="0" align="absmiddle" /></a> 
+  <a href="#" class="SocialButtons addthis_button_twitter"><img src="images/socialIcons/48x48/twitter.png" alt="Twitter" width="24" height="24" border="0" align="absmiddle" /></a> 
+  <a href="#" class="SocialButtons addthis_button_digg"><img src="images/socialIcons/48x48/digg.png" alt="Digg" width="24" height="24" border="0" align="absmiddle" /></a> 
+  <a href="#" class="SocialButtons addthis_button_delicious"><img src="images/socialIcons/48x48/delicious.png" alt="Delicous" width="24" height="24" border="0" align="absmiddle" /></a> 
+  <a href="#" class="SocialButtons addthis_button_email"><img src="images/socialIcons/mail.png" alt="Email" width="24" height="24" border="0" align="absmiddle" /></a> 
+  <a href="#" class="SocialButtons addthis_button_expanded"><img src="images/socialIcons/more.png" alt="More..." width="24" height="24" border="0" align="absmiddle" /></a>
+                <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js?pub=xa-4a85970001daba92"></script>
+                <!-- AddThis Button END -->
+              </div>
+            </div>
+            <div id="LogoDiv"><a href="/default.asp"><img src="/images/fbooklogotrial1.jpg" alt="Logo" border="0" tyle="margin-left:10px;" /></a></div></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td>
+<div id="myslidemenu" class="jqueryslidemenu">
+<ul>
+  
+  <li><a href="Bowling-Parties.asp?Page=Parties" >Parties</a>
+<ul><li><a href="bowling.asp?Page=Corporate+Meetings">Corporate Meetings</a></li><li><a href="bowling.asp?Page=Birthday+Parties">Birthday Parties</a></li></ul>
+    
+</li>
+
+  <li><a href="Bowling-Coupon.asp?Page=Coupons" >Coupons</a>
+
+    
+</li>
+
+  <li><a href="leagues.asp?Page=Bowling" >Bowling</a>
+
+    
+</li>
+
+  <li><a href="Tournaments.asp?Page=League%2FTournaments" >League/Tournaments</a>
+
+    
+</li>
+
+  <li><a href="Calendar.asp?Page=Events" >Events</a>
+
+    
+</li>
+
+  <li><a href="ContactUs.asp?Page=Contact+Us" >Contact Us</a>
+
+    
+</li>
+
+  <li><a href="bowling.asp?Page=Movies" >Movies</a>
+<ul><li><a href="bowling.asp?Page=Insidious%3A+Chapter+2">Insidious: Chapter 2</a></li><li><a href="bowling.asp?Page=Mortal+Instruments%3A++City+of+Bones">Mortal Instruments:  City of Bones</a></li><li><a href="bowling.asp?Page=Percy+Jackon%3A++Sea+of+Monsters">Percy Jackon:  Sea of Monsters</a></li><li><a href="bowling.asp?Page=Planes">Planes</a></li><li><a href="bowling.asp?Page=We%27re+the+Millers">We're the Millers</a></li></ul>
+    
+</li>
+
+</ul>
+<br style="clear: left" />
+</div>
+
+
+</td>
+  </tr>
+  <tr>
+    <td class="nav_underBG"><table width="100%" border="0" cellspacing="0" cellpadding="4" id="nav_under">
+        <tr>
+          <td nowrap="nowrap" class="NavUnderPadLeft" style="font-size:10px">409 West 3rd Street&nbsp;Mountain Grove, MO&nbsp;65711</td>
+          <td align="center"><a href="/emailSignup.asp"><img src="/images/Email-2.png" alt="Email Club" width="60" height="60" hspace="6" border="0" align="absmiddle" class="Envelope" /><span style="padding-left:70px; font-size:13px;">Join our Email Club for FREE Offers and More.</span></a></td>
+          
+          <td width="200" align="right" nowrap="nowrap" style="padding-right:10px;"><div id="SocialBookmarks">
+  Share: 
+  <a href="#" class="SocialButtons addthis_button_facebook"><img src="images/socialIcons/48x48/facebook.png" alt="Facebook" width="24" height="24" border="0" align="absmiddle" /></a> 
+  <a href="#" class="SocialButtons addthis_button_twitter"><img src="images/socialIcons/48x48/twitter.png" alt="Twitter" width="24" height="24" border="0" align="absmiddle" /></a> 
+  <a href="#" class="SocialButtons addthis_button_digg"><img src="images/socialIcons/48x48/digg.png" alt="Digg" width="24" height="24" border="0" align="absmiddle" /></a> 
+  <a href="#" class="SocialButtons addthis_button_delicious"><img src="images/socialIcons/48x48/delicious.png" alt="Delicous" width="24" height="24" border="0" align="absmiddle" /></a> 
+  <a href="#" class="SocialButtons addthis_button_email"><img src="images/socialIcons/mail.png" alt="Email" width="24" height="24" border="0" align="absmiddle" /></a> 
+  <a href="#" class="SocialButtons addthis_button_expanded"><img src="images/socialIcons/more.png" alt="More..." width="24" height="24" border="0" align="absmiddle" /></a> </div>
+
+<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js?pub=xa-4a85970001daba92"></script></td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+
+      <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" id="cTable">
+        <!--DWLayoutTable-->
+        <tr>
+          <td valign="top" class="sideNav_color">
+
+<table width="100%" border="0" cellpadding="0" cellspacing="0" class="imageLinkTable">
+  
+      <tr>
+        <td class="imageLinks"><a href="bowling.asp?Page=Movies"><img src="/images/imageLinks/movies.png" alt="Movies" border="0" /></a></td>
+      </tr>
+      
+  <tr>
+    <td valign="top">
+        <table width="100%" border="0" cellspacing="0" cellpadding="0" id="sideNavLower">
+          <tr>
+            <td>
+          
+            <a href="bowling.asp?Page=Cross+X+Roads+Grill" >Cross X Roads Grill</a>
+            
+            <a href="bowling.asp?Page=Kids+Bowl+Free" >Kids Bowl Free</a>
+            
+            <a href="leagues.asp?Page=Bowling" >Bowling</a>
+                
+            <a href="YouthPrograms.asp?Page=Youth" >Youth</a>
+                
+            <a href="bowling.asp?Page=Videos" >Videos</a>
+            
+            <a href="Specials.asp?Page=Specials" >Specials</a>
+                
+            <a href="bowling.asp?Page=ConnXtion+Collectibles" >ConnXtion Collectibles</a>
+            
+            <a href="bowling.asp?Page=Join+Email+List" >Join Email List</a>
+            
+            <a href="Bowling-Coupon.asp?Page=Coupons" >Coupons</a>
+                
+            <a href="Calendar.asp?Page=Events" >Events</a>
+                
+          </td>
+          </tr>
+        </table>
+    </td>
+  </tr>
+</table>
+<div class="sideNavLowerImage"></div>
+
+          
+          </td>
+          <td valign="top" class="ContentArea" width="100%">
+          
+		  <p>
+		  <div>
+	<span style="font-size:16px;"><span style="font-family: times new roman,times,serif;">We offer bowling, movie theater for the latest movies, a full service restaurant, bar, birthday parties, and facilities for business meetings or other gatherings. Future plans include a skating rink and a club featuring live music on Friday and Saturday nights! Whether you are looking for a league night, watching the latest movie, or having a party, ConnXtion Entertainment has something for everyone!</span></span></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script><div>
+	&nbsp;</div>
+<div>
+	&nbsp;</div>
+<div>
+	&nbsp;</div>
+<div id="fb-root">
+	<span style="font-size:16px;"><span style="font-family: times new roman,times,serif;">Like us on Facebook!</span></span></div>
+<p>
+	<fb:like href="http://www.facebook.com/ConnXtionEntertainment" send="true" show_faces="true" width="450"></fb:like></p>
+
+          </p>
+          <div style="clear:both"></div>
+            <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+              
+              <tr>
+                <td><a href="spacer"><img src="images/spacer.gif" height="2" /></a></td>
+              </tr>
+              
+                <tr>
+                  <td valign="top"><div class="title_Bbg">
+                      <h3>News</h3></div>
+					  <div class="paragraph"><p>
+	Welcome to the new website here at ConnXtion Entertainment.&nbsp; We are currently updating and changing some of our features, so keep coming back to see the latest news and updates.&nbsp; If you would like more information about our complex, special offers, or event listings at ConnXtion Entertainment, join our email club.</p>
+<p>
+	We are currently adding new things here at ConnXtion Entertainment.&nbsp; We will be finishing our construction of the skating rink and a new dance club.</p>
+<p>
+	If you are looking for something to do come visit our complex and have some fun with your friends.</p>
+</div>
+                        </td>
+                </tr>
+                
+              <tr>
+                <td valign="top">&nbsp;</td>
+              </tr>
+          </table></td>
+        </tr>
+        <tr>
+          <td colspan="4" valign="middle"><table width="100%" border="0" cellspacing="0" cellpadding="6">
+  <tr>
+    <td align="right" valign="middle" class="footer">
+    <div style="float:left; width:300px; text-align:left;"><a href="/PrivacyPolicy.asp">Privacy Policy</a> | <a href="/TermsOfUse.asp">Terms of Use</a></div>
+    &copy;&nbsp;ConnXtion Entertainment&nbsp;2014 All rights reserved</td>
+  </tr>
+</table>
+</td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+
+</body>
+</html>
